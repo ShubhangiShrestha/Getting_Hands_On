53ecf71e66941aa8ee7bf7eb27044010decd11aa
Shubhangi Shrestha <shubhangish38@gmail.com>
2023-10-10 22:38:05 +0530

    Added RnaTranscription java file

---
+++ RnaTranscription.java
@@ -0,0 +1,20 @@
+class RnaTranscription {
+
+    String transcribe(String dnaStrand) {
+        if(dnaStrand.length()==0)
+            return dnaStrand;
+        String rnaStrand="";
+        for(int i=0;i<dnaStrand.length();i++){
+            if(dnaStrand.charAt(i)=='G')
+                rnaStrand+="C";
+            else if(dnaStrand.charAt(i)=='C')
+                rnaStrand+="G";
+            else if(dnaStrand.charAt(i)=='T')
+                rnaStrand+="A";
+            else if(dnaStrand.charAt(i)=='A')
+                rnaStrand+="U";
+        }
+        return rnaStrand;
+    }
+
+}
